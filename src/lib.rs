67bff20c12b87a161ebcf25bec4e550c17e2b709
Luke Lorenzini <luke.lorenzini@gmail.com>
2024-07-22 10:46:23 -0400

    Add decompress and more tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,94 @@
 use std::collections::HashMap;
 
-pub struct Thing;
+// #[derive(Default)]
+pub struct LZW {
+    compression_map: HashMap<String, u32>,
+    decompression_map: HashMap<u32, String>,
+}
 
-impl Thing {
-    pub fn stuff(message: Vec<u8>) -> Vec<u32> {
-        let mut my_map = HashMap::new();
+impl Default for LZW {
+    fn default() -> Self {
+        let mut compression_map = HashMap::new();
+        let mut decompression_map = HashMap::new();
 
         (0..255).for_each(|i| {
-            my_map.insert((char::from(i)).to_string(), i as u32);
+            compression_map.insert((char::from(i)).to_string(), i as u32);
+            decompression_map.insert(i as u32, (char::from(i)).to_string());
         });
-        // println!("{:?}", my_map);
 
-        let mut last_entry = 256;
+        LZW {
+            compression_map,
+            decompression_map,
+        }
+    }
+}
+
+impl LZW {
+    pub fn new(
+        compression_map: HashMap<String, u32>,
+        decompression_map: HashMap<u32, String>,
+    ) -> Self {
+        LZW {
+            compression_map,
+            decompression_map,
+        }
+    }
+
+    pub fn decompress(&mut self, message: Vec<u32>) -> String {
+        let mut last_entry = self.decompression_map.len() as u32;
+        let mut ocode = message[0];
+        // print!("{:?}", char::from_u32(ocode).unwrap());
+        let mut s = String::default();
+        let mut res: String = format!("{}", char::from_u32(ocode).unwrap());
+
+        let mut c = char::default();
+
+        message.into_iter().skip(1).for_each(|ncode| {
+            // println!("{:?}", ncode);
+            // let new = ncode;
+            // let sc = format!("{}{}", s, c as char);
+            // println!("{:?}", sc);
+
+            match self.decompression_map.get(&ncode) {
+                Some(v) => {
+                    // println!("{}\t{}\t{}\tY\tx", s, c, sc);
+                    // s = format!("{}{}", s, c as char);
+                    // println!("{} found: {:?}", ncode, v);
+                    s = v.to_string();
+                }
+                None => {
+                    // res.push(*self.compression_map.get(s.as_str()).expect("Table pre-allocated"));
+                    // println!("{}\t{}\t{}\tN\t{}", s, c, sc, my_map.get(s.as_str()).unwrap());
+                    // last_entry += 1;
+                    // self.compression_map.insert(sc, last_entry);
+                    // s = (c as char).to_string();
+                    s = char::from_u32(ocode).unwrap().to_string();
+                    s = format!("{}{}", s, c);
+                }
+            };
+            // print!("{:?}", s);
+            // res.push(s.to_string());
+            res = format!("{}{}", res, s);
+            c = s.chars().nth(0).unwrap();
+            last_entry += 1;
+            self.decompression_map.insert(
+                last_entry,
+                format!(
+                    "{}{}",
+                    self.decompression_map.get(&ocode).unwrap(),
+                    c.to_owned()
+                ),
+            );
+            ocode = ncode;
+        });
+
+        // res.push(*self.compression_map.get(s.as_str()).expect("Table pre-allocated"));
+
+        res
+    }
+
+    pub fn compress(&mut self, message: Vec<u8>) -> Vec<u32> {
+        let mut last_entry = self.compression_map.len() as u32;
         let mut s = (message[0] as char).to_string();
         // println!("{:?}", s);
 
@@ -25,49 +102,91 @@ impl Thing {
                 let sc = format!("{}{}", s, c as char);
                 // println!("{:?}", sc);
 
-                match my_map.get(&sc) {
+                match self.compression_map.get(&sc) {
                     Some(_) => {
                         // println!("{}\t{}\t{}\tY\tx", s, c, sc);
                         s = format!("{}{}", s, c as char);
                     }
                     None => {
-                        res.push(*my_map.get(s.as_str()).expect("Table pre-allocated"));
+                        res.push(
+                            *self
+                                .compression_map
+                                .get(s.as_str())
+                                .expect("Table pre-allocated"),
+                        );
                         // println!("{}\t{}\t{}\tN\t{}", s, c, sc, my_map.get(s.as_str()).unwrap());
                         last_entry += 1;
-                        my_map.insert(sc, last_entry);
+                        self.compression_map.insert(sc, last_entry);
                         s = (c as char).to_string();
                     }
                 };
             });
 
-        res.push(*my_map.get(s.as_str()).expect("Table pre-allocated"));
+        res.push(
+            *self
+                .compression_map
+                .get(s.as_str())
+                .expect("Table pre-allocated"),
+        );
+
+        // println!("{:?}", self.compression_map);
 
         res
+    }
+
+    pub fn calculate_compression_ratio(&self, uncompressed: u32, compressed: u32) -> f64 {
+        compressed as f64 / uncompressed as f64
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::Thing;
+    use super::LZW;
 
     #[test]
-    fn wikipedia() {
+    fn compress_wikipedia_example() {
         let message = Vec::from("TOBEORNOTTOBEORTOBEORNOT");
-        let result = Thing::stuff(message);
+        let mut thing = LZW::default();
+        let result = thing.compress(message);
 
         let expected_result = vec![
-            84, 79, 66, 69, 79, 82, 78, 79, 84, 257, 259, 261, 266, 260, 262, 264,
+            84, 79, 66, 69, 79, 82, 78, 79, 84, 256, 258, 260, 265, 259, 261, 263,
         ];
 
         assert_eq!(expected_result, result);
     }
 
     #[test]
-    fn single_char() {
+    fn decompress_wikipedia_example() {
+        let message = vec![
+            84, 79, 66, 69, 79, 82, 78, 79, 84, 256, 258, 260, 265, 259, 261, 263,
+        ];
+        let mut thing = LZW::default();
+        let result = thing.decompress(message);
+
+        let expected_result = String::from("TOBEORNOTTOBEORTOBEORNOT");
+
+        assert_eq!(expected_result, result);
+    }
+
+    #[test]
+    fn compress_single_char() {
         let message = Vec::from("a");
-        let result = Thing::stuff(message);
+        let mut thing = LZW::default();
+        let result = thing.compress(message);
 
         let expected_result = vec![97];
+
+        assert_eq!(expected_result, result);
+    }
+
+    #[test]
+    fn decompress_single_char() {
+        let message = vec![97];
+        let mut thing = LZW::default();
+        let result = thing.decompress(message);
+
+        let expected_result = String::from("a");
 
         assert_eq!(expected_result, result);
     }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,83 @@
-use demo::Thing;
+use demo::LZW;
 
+use std::collections::HashMap;
 use std::fs;
 
 fn main() {
-    // let message = fs::read_to_string("./input_text").expect("Unable to read file");
     let message = fs::read("./input_text").expect("Unable to read file");
 
-    let res = Thing::stuff(message);
+    // let (comp, decomp) = build_custom_maps();
+    // let mut thing = LZW::new(comp, decomp);
+    let mut thing = LZW::default();
 
+    // let uncompressed_size = message.len() as u32;
+    let res = thing.compress(message);
     println!("{:?}", res);
+    // let compressed_size = res.len() as u32;
+    // let x = thing.calculate_compression_ratio(uncompressed_size, compressed_size);
+    // println!("{:?}", x);
+
+    let res = thing.decompress(res);
+    println!("{:?}", res);
+}
+
+fn _build_custom_maps() -> (HashMap<String, u32>, HashMap<u32, String>) {
+    let mut compression_map = HashMap::new();
+    let mut decompression_map = HashMap::new();
+
+    compression_map.insert("A".to_owned(), 1);
+    compression_map.insert("B".to_owned(), 2);
+    compression_map.insert("C".to_owned(), 3);
+    compression_map.insert("D".to_owned(), 4);
+    compression_map.insert("E".to_owned(), 5);
+    compression_map.insert("F".to_owned(), 6);
+    compression_map.insert("G".to_owned(), 7);
+    compression_map.insert("H".to_owned(), 8);
+    compression_map.insert("I".to_owned(), 9);
+    compression_map.insert("J".to_owned(), 10);
+    compression_map.insert("K".to_owned(), 11);
+    compression_map.insert("L".to_owned(), 12);
+    compression_map.insert("M".to_owned(), 13);
+    compression_map.insert("N".to_owned(), 14);
+    compression_map.insert("O".to_owned(), 15);
+    compression_map.insert("P".to_owned(), 16);
+    compression_map.insert("Q".to_owned(), 17);
+    compression_map.insert("R".to_owned(), 18);
+    compression_map.insert("S".to_owned(), 19);
+    compression_map.insert("T".to_owned(), 20);
+    compression_map.insert("U".to_owned(), 21);
+    compression_map.insert("V".to_owned(), 22);
+    compression_map.insert("W".to_owned(), 23);
+    compression_map.insert("X".to_owned(), 24);
+    compression_map.insert("Y".to_owned(), 25);
+    compression_map.insert("Z".to_owned(), 26);
+
+    decompression_map.insert(1, "A".to_owned());
+    decompression_map.insert(2, "B".to_owned());
+    decompression_map.insert(3, "C".to_owned());
+    decompression_map.insert(4, "D".to_owned());
+    decompression_map.insert(5, "E".to_owned());
+    decompression_map.insert(6, "F".to_owned());
+    decompression_map.insert(7, "G".to_owned());
+    decompression_map.insert(8, "H".to_owned());
+    decompression_map.insert(9, "I".to_owned());
+    decompression_map.insert(10, "J".to_owned());
+    decompression_map.insert(11, "K".to_owned());
+    decompression_map.insert(12, "L".to_owned());
+    decompression_map.insert(13, "M".to_owned());
+    decompression_map.insert(14, "N".to_owned());
+    decompression_map.insert(15, "O".to_owned());
+    decompression_map.insert(16, "P".to_owned());
+    decompression_map.insert(17, "Q".to_owned());
+    decompression_map.insert(18, "R".to_owned());
+    decompression_map.insert(19, "S".to_owned());
+    decompression_map.insert(20, "T".to_owned());
+    decompression_map.insert(21, "U".to_owned());
+    decompression_map.insert(22, "V".to_owned());
+    decompression_map.insert(23, "W".to_owned());
+    decompression_map.insert(24, "X".to_owned());
+    decompression_map.insert(25, "Y".to_owned());
+    decompression_map.insert(26, "Z".to_owned());
+
+    (compression_map, decompression_map)
 }
